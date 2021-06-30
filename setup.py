03b99b62c558d864647a5c0f31a1386baa81f5f7
hogiahien <86543292+hogiahien@users.noreply.github.com>
2021-06-30 22:01:36 +0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = "ColabVPSButKDE.py",
     version = "beta 0.1.69.420",
-    py_modules = ['ColabVPSButKDE'],
+    py_modules = ['remocolab'],
     url = "https://github.com/hogiahien/ColabVPSButKDE",
     author = "your mom",
     install_requires = ["pyngrok"]
